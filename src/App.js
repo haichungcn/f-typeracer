7369370d922e20a4f61225531f7980ff76924967
Khoa Dam <admin@Keis-MBP.local>
2019-09-17 07:43:45 +0700

    add footer text

--- src/App.js
+++ src/App.js
@@ -258,7 +258,7 @@ class Footer extends React.Component {
       <div className="footer">
         Source code available on <a target="_blank" href="https://github.com/sinapinto/react-typing-test">Github</a>.
         Colorscheme used is <a target="_blank" href="https://github.com/morhetz/gruvbox">Gruvbox</a>.
-        Retouch by K
+        Retouch by <a target="_blank" href="https://github.com/kyakaze/react_typing_test_retouch"> kyakaze :) </a>
       </div>
     );
   }
